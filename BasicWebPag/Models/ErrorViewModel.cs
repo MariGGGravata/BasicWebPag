10a3503532024f59f22bc8c5ba202418bb952831
MariGravata <marig.gravata@gmail.com>
2025-05-19 18:11:46 -0300

    Initial Project

---
+++ BasicWebPag/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasicWebPag.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
